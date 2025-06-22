19df53fcdda1bf3e324d02c03d759607e9e709f8
pdrzan <pedeozanelato19@gmail.com>
2025-06-22 19:30:38 -0300

    fix(docker-compose): remove password necessity

--- server/config.py
+++ server/config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:{os.environ.get('POSTGRES_PASSWORD')}@database:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:pdrzan@database:5432/postgres"
