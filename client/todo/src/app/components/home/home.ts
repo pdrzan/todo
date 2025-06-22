863489a5062646817697f6846bf1a4d2bfdfcc14
pdrzan <pedeozanelato19@gmail.com>
2025-06-22 14:18:07 -0300

    feat(home): add tasks rendering

--- client/todo/src/app/components/home/home.ts
+++ client/todo/src/app/components/home/home.ts
@@ -1,12 +1,22 @@
 import { Component, inject } from '@angular/core';
-import { TodoService } from '../../services/todo.service'
+import { TodoService } from '../../services/todo.service';
+import { Task } from '../../models/task';
+import { TaskComponent } from '../task-component/task-component';
 
 @Component({
   selector: 'home',
+  imports: [TaskComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-  todoSercice: TodoService = inject(TodoService);
+  todoService: TodoService = inject(TodoService);
 
+  tasks: Task[] = []
+
+  constructor() {
+    this.todoService.getAllTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    });
+  }
 }
