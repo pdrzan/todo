dc70a3838e14e1d64174d686c7c1838294f59414
pdrzan <pedeozanelato19@gmail.com>
2025-06-22 13:47:44 -0300

    feat(todo.service): add getAllTasks

--- client/todo/src/app/services/todo.service.ts
+++ client/todo/src/app/services/todo.service.ts
@@ -1,12 +1,18 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  readonly baseUrl = 'https://localhost:8000';
+  readonly baseUrl = 'http://localhost:8000';
   readonly basePath = this.baseUrl + '/task';
 
   private http = inject(HttpClient);
+
+  public getAllTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.basePath);
+  }
 }
