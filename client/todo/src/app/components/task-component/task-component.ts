ae2ae4a2f25214f0b0886bd2216b394564862ef2
pdrzan <pedeozanelato19@gmail.com>
2025-06-22 15:00:36 -0300

    feat(task-component): add completed change

--- client/todo/src/app/components/task-component/task-component.ts
+++ client/todo/src/app/components/task-component/task-component.ts
@@ -1,16 +1,26 @@
 import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Task } from '../../models/task'
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'task-component',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-component.html',
   styleUrl: './task-component.css'
 })
 export class TaskComponent {
+  todoService: TodoService = inject(TodoService);
+
   @Input()
   task: Task | undefined;
+
+  onCompleteChange(task: Task) {
+    this.todoService.updateTask(task).subscribe(task => {
+      this.task = task;
+    });
+  }
 
   constructor() {
     console.log(this.task);
